e05b1c0452ef78bfc6ec55b736ea0c32c3d25b26
Yannick <44966307+bleyyannick@users.noreply.github.com>
2024-03-22 10:51:50 +0100

    feat: add routing for pages in main.tsx (#2)

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
+import { Home } from './pages/Home/Home.tsx';
+import { Detail } from './pages/Detail/Detail.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "job_detail/:id", 
+    element: <Detail />
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+     <RouterProvider router={router} />
   </React.StrictMode>,
 )
