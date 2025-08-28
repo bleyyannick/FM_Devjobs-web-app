a4614d37ebde7e215a8b9bb96e56cf0d12330dc6
Yannick Bley <bleyyannickpro@gmail.com>
2025-08-28 11:19:42 +0200

    Revert "chore: simplify form component"
    This reverts commit 9f344020dc90851f901c9b25946958e9ab69ddac.

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,4 +1,4 @@
-import {  ComponentPropsWithoutRef, FormEvent } from 'react'; 
+import {  ComponentPropsWithRef, FormEvent } from 'react'; 
 import './Form.css'; 
 export type FilterProps = {
   title: string, 
@@ -7,34 +7,31 @@ export type FilterProps = {
 };
 
 
-export type FormProps = ComponentPropsWithoutRef<'form'> & {
+export type FormProps = ComponentPropsWithRef<'form'> & {
   onFilter : (e: FormEvent, value: FilterProps) => void;
 };
 
-
-export const Form = ({onFilter, children }: {
+export const Form = ({onFilter, children, ref}: {
   onFilter: (e: FormEvent, value: FilterProps) => void;
   children: React.ReactNode;
+  ref: React.Ref<HTMLFormElement>;
 }) => {
-
-  const formatData = (data: FormData) => {
-    return {
-      title: data.get('title')?.toString().toLowerCase() || '',
-      location: data.get('location')?.toString().toLowerCase() || '',
-      fulltime: data.get('fulltime') ? true : false
-    }
-  }
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
-    const filters = formatData(formData)
+    const formatedData = Object.fromEntries(formData.entries())
+    const filters = {
+      title: formatedData.title.toString().toLocaleLowerCase(),
+      location: formatedData.location.toString().toLocaleLowerCase(),
+      fulltime: formatedData.fulltime ? true : false
+    }
     onFilter(e,filters);
-    e.currentTarget.reset();
+ 
   }
 
     return (
-         <form onSubmit={handleSubmit}>
+         <form onSubmit={handleSubmit} ref={ref}>
           {children}
          </form>
     )
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,4 @@
-import { FC, FormEvent, useState } from "react"; 
+import { FC, FormEvent, useRef, useState } from "react"; 
 import data from '../../../data.json'; 
 import './Home.css';
 import { Header } from "../../components/Header/Header";
@@ -11,6 +11,7 @@ import iconFilter from '/mobile/icon-filter.svg'
 
 export const Home: FC = () => {
   const [jobs, setJobs] = useState<JobListProps["jobs"]>(data); 
+  const formRef = useRef<HTMLFormElement>(null);
 
   const normalizeJob = (job: Job) => ({
     position: job.position.toLowerCase().split(" "),
@@ -49,12 +50,13 @@ export const Home: FC = () => {
   const handleFilterJobs = (e: FormEvent, extractedData: FilterProps) => {
     e.preventDefault();
     setJobs(updateJobs(extractedData, [...data]));
+    formRef.current?.reset(); 
   };
 
   return (
     <>
       <Header />
-      <Form onFilter={handleFilterJobs} >
+      <Form onFilter={handleFilterJobs} ref={formRef}>
         <div className='form-filter-title'>
           <img src={iconSearch} />
           <input type='text' name="title" placeholder="Filter by title, companies, expertise.." />
